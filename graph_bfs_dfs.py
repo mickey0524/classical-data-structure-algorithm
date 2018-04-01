5fd322133990f73d2483d746ffce120fb653007c
mickey0524 <buptbh@163.com>
2018-04-01 14:44:01 +0800

    feat(graph): 增加图的bfs和dfs遍历

---
+++ graph_bfs_dfs.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: mickey0524
+# 图的bfs和dfs遍历
+# 2018-04-01
+
+from collections import deque
+
+class Graph(object):
+  def __init__(self, graph):
+    """
+    init
+    type graph: list[list] 表示图连通的二维数组
+    """
+    self.graph = graph
+
+  def bfs(self):
+    """
+    bfs遍历
+    """
+    def resursive(begin):
+      """
+      利用队列进行遍历
+      :type begin: int 当前遍历的点
+      """
+      length = len(self.graph)
+      queue = deque()
+      queue.appendleft(begin)
+      while len(queue) != 0:
+        cnt_p = queue.pop()
+        if is_visited[cnt_p]:
+          continue
+        is_visited[cnt_p] = True
+        print cnt_p,      
+        for i in xrange(length):
+          if self.graph[cnt_p][i] and not is_visited[i]:
+            queue.appendleft(i)
+      
+    length = len(self.graph)
+    is_visited = [False] * length
+
+    for i in xrange(length):
+      if not is_visited[i]:
+        resursive(i)
+      
+  def dfs(self):
+    """
+    dfs遍历
+    """
+    def resursive(begin):
+      """
+      遍历函数
+      :type begin: int 当前遍历的点
+      """
+      length = len(self.graph)
+      print begin,
+      is_visited[begin] = True
+      for i in xrange(length):
+        if self.graph[begin][i] and not is_visited[i]:
+          resursive(i)
+ 
+    length = len(self.graph)
+    is_visited = [False] * length
+    
+    for i in xrange(length):
+      if not is_visited[i]:
+        resursive(i)
+
+
+if __name__ == '__main__':
+  graphArr = [
+    [ 0, 1, 1, 0, 0 ],
+    [ 0, 0, 1, 0, 1 ],
+    [ 0, 0, 1, 0, 0 ],
+    [ 1, 1, 0, 0, 1 ],
+    [ 0, 0, 1, 0, 0 ]
+  ]
+  graph = Graph(graphArr)
+
+  print 'bfs遍历: ',
+  graph.bfs()
+  print '\ndfs遍历: ',
+  graph.dfs()
+    
+  
